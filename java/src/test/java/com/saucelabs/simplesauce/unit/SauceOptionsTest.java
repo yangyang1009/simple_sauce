6b6858e972a3acbc44e0936dbb0d4bcd9be4efb3
joshgrant <joshua.m.grant@gmail.com>
2019-10-01 10:28:49 -0400

    first test passing

--- java/src/test/java/com/saucelabs/simplesauce/unit/SauceOptionsTest.java
+++ java/src/test/java/com/saucelabs/simplesauce/unit/SauceOptionsTest.java
@@ -14,6 +14,8 @@ public class SauceOptionsTest {
     public void defaultValues(){
         sauceOptions = new SauceOptions();
 
-        assertEquals(sauceOptions.getBrowserName(), "chrome");
+        assertEquals(sauceOptions.getBrowserName(), "Chrome");
+        assertEquals(sauceOptions.getBrowserVersion(), "latest");
+        assertEquals(sauceOptions.getPlatformName(), "Windows 10");
     }
 }
