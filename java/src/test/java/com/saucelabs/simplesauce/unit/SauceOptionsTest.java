1950741eb97dd58ec495a7ad2fc82a9937f5319e
joshgrant <joshua.m.grant@gmail.com>
2019-10-01 10:39:31 -0400

    basic idea is there

--- java/src/test/java/com/saucelabs/simplesauce/unit/SauceOptionsTest.java
+++ java/src/test/java/com/saucelabs/simplesauce/unit/SauceOptionsTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Ignore;
 import static org.junit.Assert.*;
 import org.junit.Test;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class SauceOptionsTest {
 
@@ -17,5 +18,24 @@ public class SauceOptionsTest {
         assertEquals(sauceOptions.getBrowserName(), "Chrome");
         assertEquals(sauceOptions.getBrowserVersion(), "latest");
         assertEquals(sauceOptions.getPlatformName(), "Windows 10");
+    }
+
+    @Test
+    public void acceptsBrowserVersionBrowserNamePlatformName() {
+        sauceOptions = new SauceOptions("Firefox", "68.0", "macOS 10.13");
+
+        assertEquals(sauceOptions.getBrowserName(), "Firefox");
+        assertEquals(sauceOptions.getBrowserVersion(), "68.0");
+        assertEquals(sauceOptions.getPlatformName(), "macOS 10.13");
+    }
+
+    @Test
+    public void acceptsMutableCababilities() {
+        throw new NotImplementedException();
+    }
+
+    @Test
+    public void acceptsDesiredCapabilities() {
+        throw new NotImplementedException();
     }
 }
